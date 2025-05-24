87dc8e1483066d832b08093a9a64c6c413d654f2
sarthak-gc <sarthak-gc@users.noreply.github.com>
2025-05-25 02:25:20 +0545

    Can Grab

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,86 +1,180 @@
 import rough from "roughjs";
 import { useEffect, useRef, useState, type MouseEvent } from "react";
+import type { RoughCanvas } from "roughjs/bin/canvas";
 
 const generator = rough.generator();
 
-interface LinePosI {
+interface RectEndI {
+  height: number;
+  width: number;
+}
+interface LinEndI {
+  X: number;
+  Y: number;
+}
+interface StartI {
+  X: number;
+  Y: number;
+}
+// rect and line as of now
+interface ShapeI {
+  start: StartI;
+  end: LinEndI | RectEndI;
+}
+
+type Element = {
+  shape: string;
   X1: number;
   Y1: number;
   X2: number;
   Y2: number;
-}
-interface RectPosI {
-  X: number;
-  Y: number;
-  width: number;
-  height: number;
-}
+};
 const App = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [linePosition, setLinePosition] = useState<LinePosI>({
-    X1: 0,
-    Y1: 0,
-    X2: 0,
-    Y2: 0,
+
+  const [linePosition, setLinePosition] = useState<ShapeI>({
+    start: {
+      X: 0,
+      Y: 0,
+    },
+    end: {
+      X: 0,
+      Y: 0,
+    },
   });
-  const [rectPosition, setRectPosition] = useState<RectPosI>({
-    X: 0,
-    Y: 0,
-    height: 0,
-    width: 0,
+  const [rectPosition, setRectPosition] = useState<ShapeI>({
+    start: {
+      X: 0,
+      Y: 0,
+    },
+    end: {
+      height: 0,
+      width: 0,
+    },
   });
+
   const [selectedShape, setSelectedShape] = useState<string>("Rectangle");
-
+  const [action, setAction] = useState<string>("Draw");
+  const [elements, setElements] = useState<Element[]>([]);
   const [isDrawing, setIsDrawing] = useState(false);
-
+  const [grabbedElement, setGrabbedElement] = useState<Element | null>(null);
   useEffect(() => {
     if (!canvasRef.current) {
       return;
     }
     // const ctx = canvasRef.current.getContext("2d");
-
     // ctx?.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-  }, [selectedShape]);
+  }, [selectedShape, elements]);
 
+  const drawElem = (rc: RoughCanvas, X2: number, Y2: number) => {
+    if (selectedShape === "Line") {
+      const { X, Y } = linePosition.start;
+      const line = generator.line(X, Y, X2, Y2);
+      rc.draw(line);
+
+      setElements((prev) => [
+        ...prev,
+        {
+          shape: selectedShape,
+          X1: X,
+          Y1: Y,
+          X2,
+          Y2,
+        },
+      ]);
+    } else if (selectedShape == "Rectangle") {
+      const { X, Y } = rectPosition.start;
+      const rect = generator.rectangle(X, Y, X2, Y2);
+      rc.draw(rect);
+      setElements((prev) => [
+        ...prev,
+        {
+          shape: selectedShape,
+          X1: X,
+          Y1: Y,
+          X2,
+          Y2,
+        },
+      ]);
+    }
+  };
+
+  const elementThere = (element: Element, x: number, y: number) => {
+    if (element.shape == "Line") {
+      const d1 = element.X2 - element.X1;
+      const n1 = element.Y2 - element.Y1;
+      if (n1 == 0 || d1 == 0) return true;
+      const slopeOfLine = n1 / d1;
+
+      const d2 = element.X1 - x;
+      const n2 = element.Y1 - y;
+      if (n2 == 0 || d2 == 0) return true;
+
+      const slopeWrtPoint = n2 / d2;
+
+      if (Math.abs(slopeWrtPoint - slopeOfLine) < 0.1) return true;
+    } else {
+      if (
+        x >= element.X1 &&
+        x <= element.X1 + element.X2 &&
+        y >= element.Y1 &&
+        y <= element.Y1 + element.Y2
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    console.log(grabbedElement);
+  }, [grabbedElement]);
   const handleMouseRelease = (e: MouseEvent) => {
     if (!canvasRef.current) {
       return;
     }
+
     const rc = rough.canvas(canvasRef.current);
 
-    if (selectedShape == "Line") {
-      setLinePosition((prev) => {
-        return { ...prev, X2: e.clientX, Y2: e.clientY };
-      });
-      const { X1, Y1 } = linePosition;
-      const line = generator.line(X1, Y1, e.clientX, e.clientY);
-      rc.draw(line);
-    } else if (selectedShape == "Rectangle") {
-      const height = e.clientY - rectPosition.Y;
-      const width = e.clientX - rectPosition.X;
-      setRectPosition((prev) => {
-        return { ...prev, height, width };
-      });
+    if (action == "Draw") {
+      if (selectedShape == "Line") {
+        drawElem(rc, e.clientX, e.clientY);
+      } else if (selectedShape == "Rectangle") {
+        const height = e.clientY - rectPosition.start.Y;
+        const width = e.clientX - rectPosition.start.X;
+        drawElem(rc, width, height);
+      }
+    } else {
+      let i;
+      let skipped = false;
+      for (i = elements.length - 1; i >= 0; i--) {
+        if (elementThere(elements[i], e.clientX, e.clientY)) {
+          skipped = true;
+          break;
+        }
+      }
 
-      const { X, Y } = rectPosition;
-      const rect = generator.rectangle(X, Y, width, height);
-      rc.draw(rect);
+      if (skipped) {
+        setGrabbedElement(elements[i]);
+      }
+      setAction("Draw");
     }
+
     setIsDrawing(false);
   };
 
   const handleMouseSelect = (e: MouseEvent) => {
     setRectPosition((prev) => {
-      return { ...prev, X: e.clientX, Y: e.clientY };
+      return { ...prev, start: { X: e.clientX, Y: e.clientY } };
     });
     setLinePosition((prev) => {
-      return { ...prev, X1: e.clientX, Y1: e.clientY };
+      return { ...prev, start: { X: e.clientX, Y: e.clientY } };
     });
     setIsDrawing(true);
   };
 
   const handleMouseMove = (e: MouseEvent) => {
-    if (isDrawing) {
+    if (isDrawing && !isDrawing) {
       // reDraw?????????
       console.log(e);
     }
@@ -92,7 +186,10 @@ const App = () => {
         <div>
           <label htmlFor="rect">Rectangle</label>
           <input
-            onChange={() => setSelectedShape("Rectangle")}
+            onChange={() => {
+              setAction("Draw");
+              setSelectedShape("Rectangle");
+            }}
             type="radio"
             value={"Rectangle"}
             id="rect"
@@ -103,11 +200,26 @@ const App = () => {
         <div>
           <label htmlFor="line">Line</label>
           <input
-            onChange={() => setSelectedShape("Line")}
+            onChange={() => {
+              setAction("Draw");
+              setSelectedShape("Line");
+            }}
             type="radio"
             value={"line"}
             id="line"
             name="shape"
+            checked={selectedShape === "Line"}
+          />
+        </div>
+        <div>
+          <label htmlFor="grab">Grab</label>
+          <input
+            onChange={() => setAction("Grab")}
+            type="radio"
+            value={"Grab"}
+            id="grab"
+            name="shape"
+            checked={action === "Grab"}
           />
         </div>
       </div>
