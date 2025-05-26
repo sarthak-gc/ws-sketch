b470b57c8ee5912cc56edf13f660e015fcd67df9
sarthak-gc <sarthak-gc@users.noreply.github.com>
2025-05-26 20:13:44 +0545

    Schema Setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,22 +2,33 @@ import WebSocket, { WebSocketServer } from "ws";
 import { v4 } from "uuid";
 
 const wss = new WebSocketServer({ port: 9000 });
+interface Element {
+  shape: string;
+  X1: number;
+  Y1: number;
+  X2: number;
+  Y2: number;
+  id: string;
+}
+let elements: Element[] = [];
+
+let connections: WebSocket[] = [];
 
 wss.on("connection", (socket: WebSocket) => {
   console.log("User connected");
-
+  connections.push(socket);
   socket.on("message", (e) => {
-    console.log(e.toString());
-
-    socket.send(
-      JSON.stringify({
-        msg: e.toString(),
-        v4,
-      })
-    );
+    elements = JSON.parse(e.toString());
+    console.log(elements);
+    connections.forEach((connection) => {
+      connection.send(JSON.stringify(elements));
+    });
   });
 
   socket.on("close", () => {
     console.log("User Disconnected");
+    connections = connections.filter((connection) => {
+      return connection != socket;
+    });
   });
 });
