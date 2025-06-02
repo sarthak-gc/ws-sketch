11b307bc0fdcbd6cbc03481a2308b189f35c6a7f
sarthak-gc <sarthak-gc@users.noreply.github.com>
2025-06-02 12:57:15 +0545

    WS - WIP-4 (structural)

--- backend/ws/src/index.ts
+++ backend/ws/src/index.ts
@@ -12,19 +12,12 @@ interface Element {
   id: string;
 }
 
-interface currentDrawingElementI {
-  [key: string]: {
-    element: Element;
-  };
-}
-
 interface WebSocketWithId extends WebSocket {
   id: string;
 }
 
-let elements: Element[]; // everything on the canvas
-
-let currentDrawingElement: Map<string, currentDrawingElementI> = new Map(); // one element that the user is drawing, to share it with other collaborators
+let elements: Map<string, Element> = new Map();
+let currentDrawingElement: Map<string, Element> = new Map();
 
 let connections: WebSocketWithId[] = [];
 
@@ -32,29 +25,46 @@ wss.on("connection", (socket: WebSocketWithId) => {
   socket.id = v4();
   console.log("User connected with id", socket.id);
   connections.push(socket);
-  socket.on("message", (e) => {
-    let msg: currentDrawingElementI;
-    msg = JSON.parse(e.toLocaleString());
-    let keys = Object.keys(msg);
 
-    let key = keys[0];
-    if (!key) {
+  // const arrayfiedElements = Array.from(elements.values());
+
+  socket.send(
+    JSON.stringify({
+      state: "existing",
+      // elements: arrayfiedElements,
+      elements,
+    })
+  );
+
+  socket.on("message", (e) => {
+    let msg;
+    msg = JSON.parse(e.toLocaleString());
+
+    const id = Object.keys(msg)[0];
+    const value = Object.values(msg)[0] as Element;
+
+    if (!id) {
       socket.send("UserId Needed");
       return;
     }
 
-    let value = msg[key];
-    if (key) {
-      currentDrawingElement.set(key, { element: value });
+    if (!elements.has(id)) {
+      currentDrawingElement.set(id, value);
     }
+
+    elements.set(value.id, value);
 
     let serializedCurrentDrawingElement = Object.fromEntries(
       currentDrawingElement.entries()
     );
 
+    const message = {
+      existingElements: elements,
+      drawingElements: serializedCurrentDrawingElement,
+    };
     connections.forEach((connection) => {
       if (connection.id != socket.id) {
-        connection.send(JSON.stringify(serializedCurrentDrawingElement));
+        connection.send(JSON.stringify(message));
       }
     });
   });
