c9f027ea51d7d3a8aedbaed759431e184b1f6087
sarthak-gc <sarthak-gc@users.noreply.github.com>
2025-06-04 22:40:47 +0545

    Rangi birangi

--- frontend/src/utils/createElem.tsx
+++ frontend/src/utils/createElem.tsx
@@ -4,11 +4,5 @@ export const createElement = (
   e: Element,
   setElement: (element: Element) => void
 ) => {
-  const { shape, X1, Y1, X2, Y2, id } = e;
-  if (shape == "Line") {
-    setElement({ shape, X1, Y1, X2, Y2, id });
-  } else if (shape == "Rectangle") {
-    setElement({ shape, X1, Y1, X2, Y2, id });
-  }
+  setElement(e);
 };
-
